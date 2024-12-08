f8c4f557a5fcf6a9aea6dd2fc780dd0db2e8d97d
Emre Ergun <emreergun@engramsoft.com>
2024-12-08 17:32:43 +0300

    typo fixed

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -1,9 +1,9 @@
 import { getOrdersWithProducts } from '@/actions/orders';
 
 const Orders = async () => {
-  const orderWithProducts = await getOrdersWithProducts();
+  const ordersWithProducts = await getOrdersWithProducts();
 
-  if (!orderWithProducts)
+  if (!ordersWithProducts)
     return (
       <div className='text-center font-bold text-2xl'>No Orders Found</div>
     );
